63f84075938686c0207fbffa6d4ab8277f087563
swapnil <swapnil130501@gmail.com>
2023-10-13 12:42:03 +0530

    socket.io setup

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('from_client', () => {
+        console.log("event coming from client");
+    })
+
+    setInterval(() => {
+        socket.emit('from_server');
+    }, 2000);
+});
+
 app.use('/', express.static(__dirname + '/public'));
 
 server.listen(3000, () => {
